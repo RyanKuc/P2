bb509314c189d913340473326fa01fe694b4280f
Ryan Kucinski <ryankucinski@gmail.com>
2015-09-21 21:53:11 -0400

    added logic for hyphen seperator and started caps, generator working

--- index.php
+++ index.php
@@ -14,14 +14,42 @@
   <form role="form-group" method='POST' action='index.php' class="form-group"></br>
     Password Generator:</br>
     <label for="numwords">Enter Number of Words: </label>
-    <input type='number' name='numwords' min='1' max='9' value="4" class="form-control" id="numwords"><br>
+    <input type='number' name='numwords' min='1' max='9' value="<?=$numwords?>" class="form-control" id="numwords"><br>
     <label for="numspec">Enter Number of Special Characters: </label>
-    <input type='number' name='numspec' min='0' max='9' value="0" class="form-control" id="numspec"><br>
+    <input type='number' name='numspec' min='0' max='5' value="<?=$numspec?>" class="form-control" id="numspec"><br>
     <label for="numnum">Enter Number of Numberic Values 0-99: </label>
-    <input type='number' name='numnum' min='0' max='9' value="0" class="form-control" id="numnum"><br>
-    <input type='reset' value='Set New Password' class="btn-block form-control">
+    <input type='number' name='numnum' min='0' max='5' value="<?=$numnum?>" class="form-control" id="numnum"><br>
+    <input type='checkbox' class="checkbox" value="<?=$allcaps?>" name="allcaps">ALL CAPS</br>
+    <input type='checkbox' class="checkbox" value="yes" name="hyphen" <?php if(isset($_POST['hyphen']) &&
+       $_POST['hyphen'] == 'yes'){echo "checked";} else {echo "";} ?>>hyphen-seperated</br>
+    <input type='submit' value='Set New Password' class="btn-block form-control">
   </form>
-</div>
+  </div>
+  <div class="col-lg-4">
+<?php
+if(isset($_POST['hyphen']) &&
+   $_POST['hyphen'] == 'yes')
+{
+    for($i = 0; $i < $total_obj; $i++){
+      if($i == $total_obj-1){
+      echo $passwordcomps[$i];
+    }
+    else {echo $passwordcomps[$i]."-";}
+    };
+
+}
+else
+{
+  foreach($passwordcomps as $value) {
+    echo $value." ";};
+}
+?>
+<?php
+foreach($passwordcomps as $value) {
+  echo strtoupper($value);};
+?>
+  </div>
+
 </div>
 </div>
 
